fc7d50db03b15e8b05dbec6294a2bd9734f27255
foreman3 <robert.j.foreman@gmail.com>
2023-08-14 17:03:58 -0400

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -16,3 +16,20 @@ egg.addCode("up,up", function() {
     console.log("Hook called for: " + this.activeEgg.keys);
     console.log(this.activeEgg.metadata);
   }).listen();
+
+$(document).ready(function() {
+  $('#yui_3_17_2_1_1692046769336_320').click(function (){
+    $('body').terminal({
+    hello: function(what) {
+        this.echo('Hello, ' + what +
+                  '. Wellcome to the button terminal.');
+    },
+    cat: function() {
+        this.echo($('<img src="https://placekitten.com/408/287">'));
+    }
+  }, {
+    greetings: 'Button Terminal'
+  });
+  });
+});
+
