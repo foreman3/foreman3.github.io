a0636d70defd59a2549f99563773b687ba4c67eb
foreman3 <robert.j.foreman@gmail.com>
2023-08-14 16:31:46 -0400

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -2,10 +2,13 @@ var egg = new Egg();
 egg
   .addCode("up,up", function() {
     $('body').terminal(
-      {
-        greetings: 'My First Web Terminal'
-      });
-  })
+    hello: function(what) {
+        this.echo('Hello, ' + what +
+                  '. Wellcome to this terminal.');
+    }
+  }, {
+    greetings: 'Relocation Services Command Line Terminal For Advanced Actions (RSCLTFAA)'
+  });
   .addHook(function(){
     console.log("Hook called for: " + this.activeEgg.keys);
     console.log(this.activeEgg.metadata);
